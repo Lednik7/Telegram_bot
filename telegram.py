41f0e0e59ba742ee7a388c097b21f205f731e0e7
Lednik7 <52196169+Lednik7@users.noreply.github.com>
2020-04-14 15:18:03 +0300

    new

---
+++ telegram.py
@@ -0,0 +1,13 @@
+import telebot
+
+bot = telebot.TeleBot("1147580820:AAHaQFebkCXVYcgzJsaBsch_kt8YD0sZx_Q")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.polling()
