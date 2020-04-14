018c229f59f2a918801796305cf87740a1f1bc99
Lednik7 <52196169+Lednik7@users.noreply.github.com>
2020-04-14 17:55:33 +0300

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -18,7 +18,7 @@ def random_text(message):
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if "погода" == (message.text).lower():
+    if "погода" == (message.text).lower() or "погода" in (message.text).lower():
         bot.send_message(message.from_user.id, "Какой населенный пункт тебе нужен?")
         bot.register_next_step_handler(message, get_weather)
         
