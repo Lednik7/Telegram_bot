60e9ea60610e1de33d06de15b31e9926c3329b26
Lednik7 <52196169+Lednik7@users.noreply.github.com>
2020-04-14 17:22:52 +0300

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -7,12 +7,11 @@ bot = telebot.TeleBot("1147580820:AAHaQFebkCXVYcgzJsaBsch_kt8YD0sZx_Q")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Вот, что я умею: \n Пиши 'погода'")
+    bot.reply_to(message, "Вот, что я умею: \n ---Пиши 'погода {название населенного пункта}'")
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if "погода" in (message.text).lower():
-        #bot.send_message(message.from_user.id, "Какой населенный пункт тебя интересует?")
         place = (message.text).lower()
         try:
             place = place[place.find(" ")+1:]
@@ -26,10 +25,16 @@ def get_text_messages(message):
             bot.send_message(message.from_user.id, "Сейчас на улице: " + w.get_detailed_status(), str(round(temp)) + "°C")
             bot.send_message(message.from_user.id, "Скорость ветра: " + str(w.get_wind()["speed"]) + " м/с")
             bot.send_message(message.from_user.id, "Влажность воздуха: " + str(w.get_humidity()) + "%")
+            
+            bot.send_message(message.from_user.id, "Обращайся, если хочешь узнать погоду)")
 
         except:
 
             bot.send_message(message.from_user.id, "Прости, но я не нашел информацию по населенному пунктку '" + place + "'")
+            
+@bot.message_handler(content_types=['text'])
+def random_text():
+    bot.send_message(message.from_user.id, "Обращайся по погоде)"
         
 bot.polling(none_stop=True, interval=0)
 
