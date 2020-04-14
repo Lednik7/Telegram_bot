06b36e1dd659d0a3e148a24ac5acb782dd57e78d
Lednik7 <52196169+Lednik7@users.noreply.github.com>
2020-04-14 16:11:23 +0300

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -2,6 +2,8 @@ import telebot
 
 bot = telebot.TeleBot("1147580820:AAHaQFebkCXVYcgzJsaBsch_kt8YD0sZx_Q")
 
+bot.send_message(message.from_user.id, "Hi")
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
@@ -10,10 +12,8 @@ def send_welcome(message):
 def get_text_messages(message):
     if (message.text).lower() == "погода":
         bot.send_message(message.from_user.id, "Yes, its true")
+	
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
 	
 
 bot.polling(none_stop=True, interval=0)
