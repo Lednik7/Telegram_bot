75552565a19e51aa4c65621cd6626128845561bf
Lednik7 <52196169+Lednik7@users.noreply.github.com>
2020-04-14 16:28:52 +0300

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,19 +1,31 @@
 import telebot
+import pyowm
+
+owm = pyowm.OWM("905c95dc8f833e9035b8f633fc478ee6", language = "ru")
 
 bot = telebot.TeleBot("1147580820:AAHaQFebkCXVYcgzJsaBsch_kt8YD0sZx_Q")
 
-bot.send_message(message.from_user.id, "Hi")
-
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
-	
+    bot.reply_to(message, "Вот, что я умею: \n Пиши 'погода'")
+
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if (message.text).lower() == "погода":
-        bot.send_message(message.from_user.id, "Yes, its true")
-	
+        bot.send_message(message.from_user.id, "Какой населенный пункт тебя интересует?")
+        try:
+            observation = owm.weather_at_place((message.text).lower())
 
-	
+            w = observation.get_weather()
 
+            temp = w.get_temperature('celsius')["temp"]
+
+            bot.send_message(message.from_user.id, "Сейчас на улице: " + w.get_detailed_status(), str(round(temp)) + "°C")
+            bot.send_message(message.from_user.id, "Скорость ветра: " + str(w.get_wind()["speed"]) + " м/с")
+            bot.send_message(message.from_user.id, "Влажность воздуха: " + str(w.get_humidity()) + "%")
+
+        except:
+
+            bot.send_message(message.from_user.id, "Прости, но я не нашел информацию по населенному пунктку '" + home + "'")
+        
 bot.polling(none_stop=True, interval=0)
