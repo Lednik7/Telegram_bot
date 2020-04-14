76bcf0514284e5b17cdacf5828089878d7682d1c
Lednik7 <52196169+Lednik7@users.noreply.github.com>
2020-04-14 15:49:41 +0300

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -9,5 +9,10 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
 	bot.reply_to(message, message.text)
+	
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.lower() == "погода":
+        print("yES")
 
 bot.polling()
