61d1b1d9a38d85a9622d992fbf8e8496d18a85c2
Lednik7 <52196169+Lednik7@users.noreply.github.com>
2020-04-15 15:44:07 +0300

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -3,10 +3,11 @@ import pyowm
 from telebot import types
 import random
 
-owm = pyowm.OWM("905c95dc8f833e9035b8f633fc478ee6", language = "ru")
+owm = pyowm.OWM("905c95dc8f833e9035b8f633fc478ee6", language = "ru") #подключаем api openweather
 
-bot = telebot.TeleBot("1147580820:AAHaQFebkCXVYcgzJsaBsch_kt8YD0sZx_Q")
+bot = telebot.TeleBot("1147580820:AAHaQFebkCXVYcgzJsaBsch_kt8YD0sZx_Q") #подключаем api telegram
 
+#подготавливаем фразы
 hi = ["Привет!", "Как делишки?", "Давно не виделись)", "Приветсвую", "Рад тебя видеть!)", "Здравствуй!"]
 
 cold = [" шапку", " теплую куртку", " джинсы"]
@@ -15,12 +16,15 @@ normal = [" кепку", " ветровку", " джинсы"]
 
 hot = [" кепку", " футболку", " легкие джинсы или шорты"]
 
+
+#функция ответа на команды
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.send_message(message.from_user.id, random.choice(hi))
     bot.reply_to(message, "Вот, что я умею: \n --- Пиши 'Погода'")
 
 
+#функция ответа на текстовое сообщение
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if "погода" == (message.text).lower():
@@ -28,12 +32,15 @@ def get_text_messages(message):
         bot.register_next_step_handler(message, get_weather)
     else:
         bot.send_message(message.from_user.id, "Я отвечаю только на вопросы о погоде)")
-        
+  
+
+#функция, выполняемая при запросе "погода"
 @bot.message_handler(content_types=['text'])
 def get_weather(message):
     global place
     place = (message.text).lower()
     global w
+    
     try:
         observation = owm.weather_at_place(place)
         
@@ -55,10 +62,13 @@ def get_weather(message):
         bot.send_message(message.from_user.id, "Хочешь узнать подробности? Если хочешь пиши 'Подробности'")
         
         bot.register_next_step_handler(message, get_weather_detailed)
+        
     except:
 
         bot.send_message(message.from_user.id, "Прости, но я не нашел информацию по населенному пунктку '" + place + "'")
         
+        
+#функция, выполняемая при запросе "погода"-"подробности"
 @bot.message_handler(content_types=['text'])
 def get_weather_detailed(message):
 
